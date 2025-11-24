cc7123b14605a6fc65628672b5e4201625e7f013
HRISTOSDF <crisstoyanov@gmail.com>
2025-11-24 07:12:38 +0200

    some changes

---
+++ main_app/mixins.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class PublishedOnMixin(models.Model):
+    published_on = models.DateTimeField(
+        auto_now_add= True,
+        editable= False
+    )
+
+    class Meta:
+        abstract = True
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,33 @@
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
+from main_app.mixins import PublishedOnMixin
+
+
 # Create your models here.
+class Author(models.Model):
+    full_name = models.CharField(
+        max_length= 3,
+        validators= [MinLengthValidator(3)]
+    )
+    email = models.EmailField(
+        unique= True
+    )
+    is_banned = models.BooleanField(
+        default= False
+    )
+    birth_year = models.PositiveIntegerField(
+        validators=[
+            MinValueValidator(1900),
+            MaxValueValidator(2005)
+        ]
+    )
+    website = models.URLField(
+        blank= True,
+        null= True
+    )
+
+class Article(PublishedOnMixin):
+    title=  models.CharField(
+
+    )
